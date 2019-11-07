38c0733fc715d4f62475efe68f65570a7d3c5f55
osamagmt <34580348+osamagmt@users.noreply.github.com>
2019-11-08 00:31:03 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,49 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const ms = require("ms");
+const moment = require("moment");
+const prefix = "±"
+const adminprefix = "#";
+
+const devs = ['ID1' , 'ID2'];
+
+
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+  } else 
+ if (message.content.startsWith(adminprefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.sendMessage(`**${argresult}** : تم تغير الأسم`)
+} else
+if (message.content.startsWith(adminprefix + 'setavatar')) {
+client.user.setAvatar(argresult);
+} else 
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/osama_gmt");
+      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+  }
+  });
+
+
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
+
+
+
+client.login(process.env.BOT_TOKEN);
